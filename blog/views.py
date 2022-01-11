a2450f39ba7e90e7bbeb66c5430722b70360f5e9
Pedro Silva <pedrocas-silva@hotmail.com>
2022-01-11 11:54:07 +0000

    Added function views. Added html templates that will be needed.

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404,redirect
 from django.utils import timezone
 from django.views.generic import TemplateView,ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from blog.models import Post,Comment
-from blog.forms import PostForm
+from blog.forms import CommentForm, PostForm
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -60,11 +61,45 @@ class DraftListView(ListView):
     def get_queryset(self):
         return Post.objects.filter(published_date__isnull=True).order_by('created_date')
      
+############################################
+############################################
+
+@login_required
+def publish_post(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    post.publish()
+    return redirect('post_detail',pk=pk)
+    
 
 
 
+@login_required
+def add_comment_to_post(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+
+    if request.method ==' POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail',pk = post.pk)
+    else:
+        form = CommentForm()
+
+    return render(request,'blog/comment_form.html',{'form':form})
 
 
+@login_required
+def comment_approve(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    comment.approve()
+    return redirect('post_detail',pk = comment.post.pk)
 
-
+@login_required
+def comment_remove(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail',pk = post_pk)
 
